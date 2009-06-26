995220847870a3783adfe88cf75c33469e06f60f
Ian Norton <inb@ncipher.com>
2009-06-26 14:58:27 +0100

    iterative implementation instead

--- csdiff/csdiff.cs
+++ csdiff/csdiff.cs
@@ -1,19 +1,11 @@
 
 using System;
-using NUnit.Framework;
-using NUnit.Core;
 using System.Collections.Generic;
 using System.Collections;
 
 namespace csdiff
 {
 	public class LCS {
-		
-		private int iterations = 0;
-		
-		public int Iterations {
-			get { return iterations; }
-		}
 		
 		public int[,] lengths;
 		
@@ -34,53 +26,26 @@ namespace csdiff
 	
 			lengths = new int[ 1 + end_a - start_a , 1 + end_b - start_b ];
 			
-			{
-				int x = end_a - start_a;
-				while ( x > 0 ){
-					--x;
-					int y = end_b - start_b;
-					while ( y > 0 )
-						lengths[x,--y] = -1;	
+			// now actually find our longest common substring (length only)
+			
+			for ( int i = ( end_a - start_a -1 ) ; i >= 0; i-- ){	
+				for ( int j = ( end_b - start_b -1 ) ; j >= 0 ; j-- ){
+					if ( a[i + start_a].Equals( b[j + start_b ] ) ){
+						lengths[i,j] = 1 + lengths[i+1,j+1];
+					} else {
+						int x = lengths[ i+1,j ];
+						if ( lengths[i,j+1] > x )
+							x = lengths[i,j+1];
+						lengths[i,j] = x;
+					}
 				}
 			}
-	
-			// now actually find our longest common substring (length only)
-			return SubLength( ref a, start_a, end_a, ref b, start_b, end_b );
+			
+			return lengths[0,0];
+			
 		}
-		
-		private int SubLength<T>( ref T [] a, int a_start, int a_end, ref T[] b, int b_start, int b_end )
-		{
-			if ( a_start >= a_end )
-				return 0;
-			if ( b_start >= b_end ) 
-				return 0;
 			
-			if ( lengths[ a_start , b_start ] > -1)
-				return lengths[ a_start , b_start ];
 		
-			iterations++;
-			int rt;
-			
-			if ( a[a_start].Equals(b[b_start]) ){
-				rt = 1 + SubLength( ref a, ++a_start, a_end, ref b, ++b_start, b_end );
-			} else {
-				int al = SubLength ( ref a, ++a_start, a_end, ref b, b_start, b_end );
-				int bl = SubLength ( ref a, a_start, a_end, ref b, ++b_start, b_end );
-				rt = bl;
-			
-				if ( al > bl )
-					rt = al;
-				
-			}
-			
-			lengths[ a_start, b_start ] = rt;
-				
-			return rt;
-		}
-		
-		
-		
-		[Test]
 		public void TestInts()
 		{
 			Console.WriteLine("Test Integers");
@@ -89,19 +54,16 @@ namespace csdiff
 			int[] bx = new int[]{ 2,1,3,3,4,5,6,7,9,1,1,2,3,5,6,6 };
 			
 			Console.WriteLine( "LCS {0} ", Length( ref ax, ref bx ) );
-			Console.WriteLine( "iterations {0}",Iterations );
 		}
 		
-		[Test]
 		public void TestStrings()
 		{
 			Console.WriteLine("Test Strings");
 			
-			string[] ax = "foo bar Baz Bob Cob Flob Zob flob flob wobble cobble Flob Flob".Split(' ');
-			string[] bx = "zoo car Baz fob Cob Flob xob xlob flob wobble wobble Flob Flob".Split(' ');
+			string[] ax = "foo bar Baz Bob Cob Flob Zob flob flob wobble cobble Flob Flob wobble floo boo moo zoo boo goo zoo too".Split(' ');
+			string[] bx = "zoo car Baz fob Cob Flob xob xlob flob wobble wobble Flob Flob xoo zoo too".Split(' ');
 			
 			Console.WriteLine( "LCS {0}" , Length( ref ax, ref bx ) );
-			Console.WriteLine( "iterations {0}", Iterations );
 		}
 		
 		
